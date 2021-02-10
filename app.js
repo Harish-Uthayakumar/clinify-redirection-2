03a9b87dd617d11bba020a72235f8165c434625a
Harish Uthayakumar <harish@Harishs-MacBook-Pro.local>
2021-02-10 22:18:32 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express')
+var app = express()
+const port = process.env.PORT || 3000
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.get('/', function(req, res) {
+
+  res.redirect("https://linktr.ee/clinify")
+})
+app.get('/whatsapp', function (req, res) {
+  res.redirect("https://chat.whatsapp.com/ImBmwYsFz6X0ulY3RgFHiI")
+})
+
+app.get('/discord', function(req,res) {
+    res.redirect("https://discord.gg/ukhrVx4kyp")
+})
+
+app.listen(port, () => {
+
+    console.log('Port is listening')
+})
