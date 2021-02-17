9bc323aaa667c0f590a457ac18b98fce2c99adac
Harish Uthayakumar <harish@Harishs-MacBook-Pro.local>
2021-02-17 12:02:28 +0530

    WhatsApp group link changed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
   res.redirect("https://www.clinify.in")
 })
 app.get('/whatsapp', function (req, res) {
-  res.redirect("https://chat.whatsapp.com/HxFmjkKHr7v2FlZO3qrcs0")
+  res.redirect("https://chat.whatsapp.com/DuZQPZgTP624WjplNMzQSA")
 })
 
 app.get('/discord', function(req,res) {
