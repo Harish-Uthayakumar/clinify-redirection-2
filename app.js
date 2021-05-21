01adfa4c4a1cf7618beef70aaa186632ef7c596e
Harish Uthayakumar <harish@Harishs-MacBook-Pro.local>
2021-05-22 01:11:48 +0530

    gg

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
   res.redirect("https://www.clinify.in")
 })
 app.get('/whatsapp', function (req, res) {
-  res.redirect("https://chat.whatsapp.com/E1tQoamvlQCFxVogt2raC8")
+  res.redirect("https://chat.whatsapp.com/EJbDXjrhY8gCNIFbDdy0ko")
 })
 
 app.get('/discord', function(req,res) {
