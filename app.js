3d89ea496d8aa08c587bbe1a1b8bcb8733a194cc
Harish Uthayakumar <harish@Harishs-MacBook-Pro.local>
2021-06-22 21:18:06 +0530

    changed dates to 25th for java

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
   res.redirect("https://www.clinify.in")
 })
 app.get('/whatsapp', function (req, res) {
-  res.redirect("https://chat.whatsapp.com/LQa9apxBecqCWpL4ZTkRGU")
+  res.redirect("https://chat.whatsapp.com/IxtzLDMZYLMBeA9aOs8rqA")
 })
 
 app.get('/discord', function(req,res) {
